7a9568dd8e544434e1394fb0e48ddad2136dde61
Nick Schuster <ngpnickschster@gmail.com>
2020-08-16 14:43:39 -0700

    Started working on basic header.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <Header />  
+  )
 }
+
+export default Home
