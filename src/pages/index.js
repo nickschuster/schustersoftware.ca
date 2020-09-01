e42fa28f00cebdd5423a50836c7ee5819a09e472
Nick Schuster <ngpnickschster@gmail.com>
2020-09-01 13:05:24 -0700

    Added scroll to top.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,20 @@ import Header from "../components/header"
 import Home from "../components/home"
 import Contact from "../components/contact"
 import Footer from "../components/footer"
-import { Element } from "react-scroll"
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 // CSS.
 import "../css/global.scss"
 
 export default function index() {
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  }
+
   return (
     <React.Fragment>
+      <div id="scroll-to-top" onClick={scrollToTop} role="a"></div>
       <div id="background">
         <div id="content-container">
           <Element name="home">
