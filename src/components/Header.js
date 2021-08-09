aea8f8ee453537ec97df5ce3b088cd86bb450b90
Nick Schuster <ngpnickschster@gmail.com>
2021-08-09 00:17:45 -0700

    Basic header layout.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,37 @@
 import React from "react"
+import Logo from "../images/icon.png"
+import { Container, Navbar, Nav } from "react-bootstrap"
 
 import "../css/header.scss"
 
 export const Header = () => {
   return (
     <>
-      <div className="header-container">
-        <div className="header-center">
-          <div className="header-logo">
-            <div className="header-image"></div>
-            <div className="header-text"></div>
-          </div>
-          <div className="header-navigation">
-            <div className="header-nav-item"></div>
-            <div className="header-nav-item"></div>
-            <div className="header-nav-item"></div>
-          </div>
-        </div>
-      </div>
+      <Navbar collapseOnSelect expand="sm">
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt=""
+              src={Logo}
+              width="75"
+              height="75"
+              className="d-inline-block align-top"
+            />{" "}
+          </Navbar.Brand>
+          <Navbar.Brand>Schuster Software</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            className="justify-content-end"
+            id="responsive-navbar-nav"
+          >
+            <Nav className="me-auto">
+              <Nav.Link>Projects</Nav.Link>
+              <Nav.Link>Contact</Nav.Link>
+              <Nav.Link id="resume-link">Resume</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   )
 }
